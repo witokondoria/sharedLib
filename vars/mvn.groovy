420c4f2bfae67b3931c731492f5ed6285434af59
Javier Delgado <witokondoria@gmail.com>
2018-03-10 09:38:06 +0100

    Create mvn.groovy

---
+++ vars/mvn.groovy
@@ -0,0 +1,15 @@
+withEnv(["PATH+MAVEN=${tool name:'3.5.2',type:'maven'}/bin","JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/"])
+{
+    //input message: 'Valid ticket, please', ok: 'Sure', parameters: [string(defaultValue: 'TICKET', description: '', name: 'JIRA', trim: true)]
+    try {
+      sh "echo mvn clean package"
+    } catch (e) {
+        //oops
+    } finally {
+      //junit '**/surefire-reports/*.xml'
+    }
+}
+
+//archive "**/*.hpi"
+}
+}
