ec0a5471d6f53439ae6ce36947a6f63b49355522
Javier Delgado <witokondoria@gmail.com>
2018-03-10 09:43:03 +0100

    Update mvn.groovy

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,4 +1,4 @@
-def package() {
+def pkg() {
 withEnv(["PATH+MAVEN=${tool name:'3.5.2',type:'maven'}/bin","JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/"])
 {
     //input message: 'Valid ticket, please', ok: 'Sure', parameters: [string(defaultValue: 'TICKET', description: '', name: 'JIRA', trim: true)]
