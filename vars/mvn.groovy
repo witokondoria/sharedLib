5ee82ee743e4d746b1fa50f3d7d67e9c2cba47ac
Javier Delgado <witokondoria@gmail.com>
2018-03-10 09:40:28 +0100

    Update mvn.groovy

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,3 +1,4 @@
+def package() {
 withEnv(["PATH+MAVEN=${tool name:'3.5.2',type:'maven'}/bin","JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/"])
 {
     //input message: 'Valid ticket, please', ok: 'Sure', parameters: [string(defaultValue: 'TICKET', description: '', name: 'JIRA', trim: true)]
@@ -11,3 +12,4 @@ withEnv(["PATH+MAVEN=${tool name:'3.5.2',type:'maven'}/bin","JAVA_HOME=/usr/lib/
 }
 
 //archive "**/*.hpi"
+}
